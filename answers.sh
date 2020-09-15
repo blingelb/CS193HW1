671ab3855a19d0245eda8bef344a3068ca9c9d60
Brayden Gregory Lingelbach <blingelb@data.cs.purdue.edu>
2020-09-15 15:01:30 -0400

    Done!

--- answers.sh
+++ answers.sh
@@ -4,42 +4,43 @@
 cd ORIGIN
 
 # Task 1 
-
+mv Homework/MA165/HW2.txt Homework/MA165/HW1.txt 
 
 # Task 2
-
+rm Memes/SpongeBob/money.jpeg
 
 # Task 3
-
+mv Memes/"Star Wars"/fafsa.jpg Memes/SpongeBob/fafsa.jpg   
 
 # Task 4
-
+rm -r Homework/CS191
 
 # Task 5
-
+mkdir Programs/C++ 
 
 # Task 6
-
+mv Programs/Age.cpp Programs/C++/Age.cpp
 
 # Task 7
-
+cp -r  Programs/Java Homework/CS180/"Project_1"  
 
 # Task 8
-
+mv Programs/Count.c Programs/C/Count.c && mv Programs/Goodbye.c Programs/C/Goodbye.c 
 
 # Task 9
-
+mv Programs/Name.py Programs/Python\ 3/Name.py && mv Programs/Space.py Programs/Python\ 3/Space.py 
 
 # Task 10
-
+touch Homework/POL141/Essay.txt
 
 # Task 11
-
+ls .. 
 
 # Task 12
 : '
 Type your vim/nano customizations below:
 
-
-
+set mouse=a
+set number
+set showmatch
 '
